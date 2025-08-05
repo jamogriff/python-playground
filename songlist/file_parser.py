cb9be9834845b903e672bd71a3a51632bc113174
Jamison Griffith <jamison.griffith@gmail.com>
2025-08-05 14:18:17 -0600

    Adds file parsing

--- songlist/file_parser.py
+++ songlist/file_parser.py
@@ -3,13 +3,18 @@ import re
 class FileParser:
     """A class to parse markdown front matter and JSON files"""
 
-    def parse_front_matter(file_contents) -> dict:
+    def parse_front_matter(self, file_contents) -> dict:
         file_lines = file_contents.readlines()
+
+        if len(file_lines) == 0:
+            raise ValueError("File is empty")
+
         match = re.search("---", file_lines.pop(0))
 
         if not match:
             raise ValueError('File does not begin with front matter')
 
+        front_matter = {}
         for line in file_lines:
             front_matter_end = re.search("---", line)
 
@@ -17,4 +22,6 @@ class FileParser:
                 break
 
             match = re.split(": ", line)
-            print(match)
+            front_matter[match[0]] = match[1].rstrip("\n")
+
+        return front_matter
--- songlist/song_service.py
+++ songlist/song_service.py
@@ -1,9 +1,35 @@
+import re
 from songlist.song import Song
+from songlist.file_parser import FileParser
 
 class SongService:
     """A service to read and write song list files"""
 
-    def get_song_from_file(file_path: str) -> Song:
-        with open(file_path, "w") as file:
-            pass
+    def __init__(self):
+        self.file_parser = FileParser()
+
+    def get_song_from_file(self, file_path: str) -> Song:
+        with open(file_path, "r") as file:
+            file_ext = self.get_file_extension(file.name)
+
+            if file_ext == ".md":
+                obj = self.file_parser.parse_front_matter(file)
+            elif file_ext == ".json":
+                # TODO
+                pass
+            else:
+                raise RuntimeError(f'Parsing {file_ext} files is not implemented')
+
+        return Song(obj['name'], obj['artist'], int(obj['skill']))
             
+
+    def get_file_extension(self, file_name) -> str:
+        """Returns file extension of a given file and will raise exception if none found.
+        Example return string: .json"""
+        match = re.search(r'\.(.+)', file_name)
+
+        if not match:
+            raise ValueError(f'{file_name} does not have a file type')
+
+        return match.group()
+
